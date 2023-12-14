439b581eabefff12c93de8bd0eda2f509ff75954
Jnanesh <jnaneshps4@gmail.com>
2023-12-14 08:01:51 +0530

    thirtenth commit

--- my-app/models/Project.js
+++
@@ -1,9 +0,0 @@
-// models/Project.js
-import mongoose from 'mongoose';
-
-const projectSchema = new mongoose.Schema({
-  name: String,
-  link: String,
-});
-
-export default mongoose.models.Project || mongoose.model('Project', projectSchema);
